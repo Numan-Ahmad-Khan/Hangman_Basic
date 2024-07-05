dc89ae3f943b17d84039f7df960872401f3f0bae
Numan-Ahmad-Khan <numan.ah.kh@gmail.com>
2024-07-05 09:35:54 +0530

    Updated

--- Hangman_code.py
+++ Hangman_code.py
@@ -1,9 +1,13 @@
+# Importing modules & other .py files for it working.
 import random
-import Hangman_words
+import Hangman_words 
 from Hangman_art import logo, win, lose, stages
 print(logo)
-a=1
-while(a==1):
+# Variable declared for Loop
+gameOn = True
+# Loop to keep the gamming running as per user choice 
+while(gameOn==True):
+# MAIN GAME CODE
      newGame = int(input("\nPress 1 to Start, Press any number key to Exit\n"))
      if(newGame == 1):
           lives = 6
@@ -13,13 +17,14 @@ while(a==1):
 #For debug purposes
           #print(f'Solution is {chosen_word}.')
 
+          # Adding dashes in code & replacing them with the correct guess
           display = []
           for _ in range(word_length):
                display += "_"
 
           while not end_of_game:
                guess = input("Guess a Letter :: ").lower()
-
+# Prompting user that he already guessed the word and no effect will take place
                if guess in display:
                     print(f"You've already guessed {guess}")
 
@@ -28,23 +33,26 @@ while(a==1):
 
                     if letter == guess:
                          display[position] = letter
-#     print(f"{' '.join(display)}")
+# Prompting user with message that a wrong letter is entered and loose a life 
                if guess not in chosen_word:
                     print(f"You guessed {guess}, thats not in the word. You lose a life")  
                     lives-=1
                     print(stages[lives])  
                     if lives == 0:
+                         # Ending the game when lives = 0
                          end_of_game = True
                          print(f"{lose}\n")
+                         # Showing user/player what the correct word was.
                          print(f"\nThe Chosen word was '{chosen_word}")
 
                print(f"{' '.join(display)}")
-
+               
+               # Ending the game with win
                if'_'not in display:
                     end_of_game=True
                     print(f"{win}\n")
 
-               # print(stages[lives])  
+# User choice to exit the game
      else: 
           print("Thank You...! Exiting...")
-          break
+          gameOn = False
